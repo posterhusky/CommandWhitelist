1d4c6d09a5daf315893de699f519304b0b03befe
Vanolex <poster.husky@gmail.com>
2023-05-14 21:27:34 +0200

    Changed the command suggestion

--- src/main/java/net/vanolex/commandwhitelist/Utils.kt
+++ src/main/java/net/vanolex/commandwhitelist/Utils.kt
@@ -5,4 +5,4 @@ import net.kyori.adventure.text.minimessage.MiniMessage
 val mm = MiniMessage.miniMessage()
 
 // adds a message when you hover the command and
-val suggestCmd = { s: String -> "<hover:show_text:'<blue><bold>Click to get /${s}'><click:suggest_command:/${s}>/${s}" }
+val suggestCmd = { s: String -> "<hover:show_text:'<blue><bold>/${s} (Click)\n<reset><dark_gray>Click to insert it in the chat'><click:suggest_command:/${s}>/${s}" }
